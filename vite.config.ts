7728b0f5ee17a07ce8c426a30192a0d2381dc26b
testops390 <testops390@users.noreply.github.com>
2026-03-05 16:58:10 +0900

    deploy docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/test-ProjectR/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  }
 });
